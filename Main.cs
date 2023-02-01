26fb4f8c56e87ffbdd4f2842c1d2b60fc7847d2b
Metalivredev <metalivredev@outlook.com>
2023-02-01 18:08:58 -0300

    Criação da classe principal

---
+++ Main.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Principal
+{
+	public PrimeiraClasse()
+	{
+		//Este é um comentario de teste.
+	}
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
 Esse arquivo ensina como usar o git e o gitHub.
+Estou alterando o arquivo.
